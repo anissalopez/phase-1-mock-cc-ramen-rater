bc28259af73350a1c49a389e52a95bca044d3118
anissalopez <anissa965@gmail.com>
2023-06-12 15:43:31 -0400

    completing first deliverable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 // write your code here
+//on page load display all images of ramen within ramen-menu div 
+window.addEventListener("load", ramenRequest);
+const imgDiv = document.querySelector('#ramen-menu');
+
+function ramenRequest(){
+    fetch('http://localhost:3000/ramens')
+    .then(response => response.json())
+    .then((data) => {
+        const ramen = data
+        ramenImages(ramen)
+    })
+}
+
+function ramenImages(ramen){
+    
+    for (let ramenObject of ramen){
+        let images = document.createElement('img')
+        images.src = ramenObject.image;
+        imgDiv.appendChild(images)
+        images.addEventListener('click', imageInfo) 
+    };
+  
+}
+
+function imageInfo(event){
+    console.log(event)
+}
